f11eff6711eb4eeb7f30dd9aa207677f6b658cf4
Grizvel32 <grizenkov1996@gmail.com>
2021-06-06 16:01:25 +0300

    add levels and etc

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,34 +10,36 @@ import java.util.Scanner;
         5.1)если число пользователя МЕНЬШЕ числа компа, то вывести подсказку что введите число больше и перейти на шаг 2
         5.2)если число пользователя БОЛЬШЕ числа компа, то вывести подсказку что введите число меньше и перейти на шаг 2
         5.3)если число пользователя РАВНО числу компа, то вывести что вы угадали и закончить программу
-
         1)Random random = new Random()
         2)System out println
         3)Scanner input NextInt
         4)if comp==Random
         5)if System out println
-
-         Доработки:
-         1) countTrying++ (записываем в переменную количество попыток, затем сделаем вывод "Вы угадали число за "" попыток)
-
-
-        */
+        5)if System out println
+          Доработки:
+          1) countTrying++ (записываем в переменную количество попыток, затем сделаем вывод "Вы угадали число за "" попыток)
+                */
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner inputLevel = new Scanner(System.in);
         Random rnd = new Random();
+
+        final int HACKNUMBER = 1306;
         int compNumber;
-        int userNumber;
+        int userNumber = 0;
         int countTrying = 0;
         int lowBoard;
         int highBoard;
         int level;
+        boolean inputResult;
 
         System.out.println("Выберите уровень сложности: \n 1 - Легкий \n 2 - Средний \n 3 - Сложный ");
 
-        level = input.nextInt();
+        //    compNumber = rnd.nextInt(100) + 1;
+
+        level = inputLevel.nextInt();
 
         switch (level) {
             case 1:
@@ -63,22 +65,36 @@ public class Main {
                 lowBoard = 1;
                 highBoard = 100;
                 break;
-
         }
         do {
-            System.out.print("Введите любое число от " + lowBoard + " до " + highBoard + ": ");
-            userNumber = input.nextInt();
+            inputResult = true;
+            Scanner input = new Scanner(System.in);
+            try {
+                System.out.print("Введите любое число от " + lowBoard + " до " + highBoard + ": ");
+                userNumber = input.nextInt();
 
-            if (userNumber > compNumber) {
-                System.out.println("Введите число поменьше ");
-            } else if (userNumber < compNumber) {
-                System.out.println("Введите число побольше ");
+            } catch (Exception e) {
+                inputResult = false;
             }
-            if (compNumber > )
+            //lowBoard = compNumber - rnd.nextInt(compNumber);
+            //highBoard = compNumber + rnd.nextInt(lowBoard);
+
+            if (userNumber == HACKNUMBER) {
+                System.out.println("ВЫ ВВЕЛИ СЕКРЕТНОЕ ЧИСЛО, ОТВЕТ: " + compNumber);
+            } else if (userNumber >= lowBoard && userNumber <= highBoard) {
+                if (userNumber > compNumber) {
+                    System.out.println("Введите число поменьше ");
+                    highBoard = userNumber;
+                } else if (userNumber < compNumber) {
+                    System.out.println("Введите число побольше ");
+                    lowBoard = userNumber;
+                }
+            }
             countTrying++;
 
-        } while (compNumber != userNumber);
+        } while (compNumber != userNumber || inputResult == false);
 
+        System.out.println("Поздравляем, вы угадали число ");
         if (countTrying >= 1 && countTrying <= 3) {
             System.out.println("Да ты гений!");
 
